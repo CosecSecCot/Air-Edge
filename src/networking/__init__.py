9c3a906a1c9f6274a91d04e0453618f619b95b5b
mad-guitartistJD <jaideepdahiyaaa@gmail.com>
2025-03-21 20:59:05 +0530

    Server implementation

--- src/networking/__init__.py
+++ src/networking/__init__.py
@@ -1,25 +1,42 @@
+import asyncio
 import websockets
+from utils.logger import server_logger 
 
-from utils.logger import server_logger
+clients = {}  
 
-
-async def get_mallet_position(websocket: websockets.ServerConnection) -> tuple[int, int]:
-    # Just an example (please implement it more elegantly)
-    x = await websocket.recv()
-    y = await websocket.recv()
-
+async def get_mallet_position(client, websocket: websockets.ClientConnection):
+    """Receives mallet position from a client and validates input."""
     try:
-        res = (int(x.strip()), int(y.strip()))
-        return res
-    except:
-        server_logger.error("Couldn't parse mallet position!")
-        return (0, 0)
+        async for message in websocket:
+            if validate_position(message):
+                server_logger.info(f"Received from Client {client}: {message}")
+            else:
+                server_logger.warning(f"Invalid input from Client {client}: {message}")
+    except websockets.exceptions.ConnectionClosedError:
+        server_logger.error(f"Client {client} disconnected unexpectedly")
+    finally:
+        server_logger.info(f"Client {client} disconnected")
+        del clients[client]
 
+def validate_position(message):
+    """Validates if the received message is a tuple of two numeric values."""
+    try:
+        x, y = map(float, message.strip("()").split(","))
+        return True 
+    except (ValueError, AttributeError):
+        return False 
+
+async def send_mallet_position():
+    """Placeholder function for sending updated mallet positions."""
+    pass  # To be implemented later
 
 async def server(websocket: websockets.ServerConnection) -> None:
-    # Just an example (please implement it more elegantly)
-    (x, y) = await get_mallet_position(websocket)
-    server_logger.debug(f'Mallet postion: ({x}, {y})')
+    """Handles client connections."""
+    client_id = len(clients) + 1  # Assign a unique ID
+    clients[client_id] = websocket
+    server_logger.info(f"Client {client_id} connected.")
+    
+    await get_mallet_position(client_id, websocket)
 
     # Do some physics with puck and mallet
     # Send the resulting position of the puck to the clients connected to the game
